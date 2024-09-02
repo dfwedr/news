ef78173a3087523fc440ffc7b477182c9d03625a
Кузнецов Николай Евгеньевич <kuznetsov@bull.local>
2024-09-02 18:37:13 +0300

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-import Header from './components/Header/Header'
-import Main from './pages/Main/Main'
-
-function App() {
-	return (
-		<>
-			<Header />
-			<div className='container'>
-				<Main />
-			</div>
-		</>
-	)
-}
-// first
-export default App
-=======
 import Header from "./components/Header/Header";
 import Main from "./pages/Main/Main";
 //новое
@@ -30,4 +13,3 @@ function App() {
 }
 
 export default App;
->>>>>>> ea64ba19376d3608f2222ea3b9e557b10b817d32
