36903afc7822768a9aeec0d62c2ba510c94ac649
Кузнецов Николай Евгеньевич <kuznetsov@bull.local>
2025-01-04 17:14:29 +0300

    fix 2

--- src/app/layouts/BaseLayout.tsx
+++ src/app/layouts/BaseLayout.tsx
@@ -1,6 +1,6 @@
 import Header from "@/widgets/header/ui/Header/Header";
 import { useTheme } from "@/app/providers/ThemeProvider";
-import MainPage from "@/pages/main/MainPage";
+import { MainPage } from "@/pages/main";
 
 function BaseLayout() {
   const { isDark } = useTheme();
